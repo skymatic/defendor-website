329737065d3fa94fdb1cdc6172f28dbea47caf8b
Tobias Hagemann <tobias@hagemann.org>
2018-05-26 18:47:03 +0200

    added german localization

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$('#request-demo-form').submit(function(event) {
+  event.preventDefault();
+
+  var $form = $(this);
+  var submitButton = $form.find('button[type=submit]');
+  submitButton.prop('disabled', true);
+
+  var email = $('#request-demo-email').val();
+
+  var url = $form.attr('action');
+  $.post(url, { email: email })
+  .done(function() {
+    $form.addClass('d-none');
+    $('#request-demo-success').removeClass('d-none');
+    $('#request-demo-error').addClass('d-none');
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.log('Failed to request demo.', textStatus, errorThrown);
+    $('#request-demo-error').removeClass('d-none');
+    submitButton.prop('disabled', false);
+  });
+});
