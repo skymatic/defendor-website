835c3ccf0f11f4f37494394da05978f37ffdcbd6
Tobias Hagemann <tobias@hagemann.org>
2018-11-02 20:05:29 +0100

    added upgrade overview and trial license upgrade

--- js/app.js
+++ js/app.js
@@ -15,6 +15,33 @@ $(document).ready(function () {
       $installationId.prop('readonly', true);
     }
   }
+
+  var $licenseId = $('#license-id');
+  if ($licenseId.length) {
+    var licenseId = getUrlParameter('license_id');
+    if (licenseId) {
+      $licenseId.val(licenseId);
+      $licenseId.prop('readonly', true);
+    }
+  }
+
+  var $upgradeTrialForm = $('#upgrade-trial-form');
+  if ($upgradeTrialForm.length && $installationId.length && $licenseId.length) {
+    var $email = $('#email');
+    $email.prop('readonly', true);
+    $.get('https://api.cryptomator.org/licenses/licenseinfo.php', {
+      installation_id: $installationId.val(),
+      license_id: $licenseId.val()
+    })
+    .done(function(data) {
+      $email.val(data.claims.sub);
+      $email.prop('readonly', false);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      console.error('Failed to get license info.', textStatus, errorThrown);
+      $email.prop('readonly', false);
+    });
+  }
 });
 
 $('#trial-form').submit(function(event) {
@@ -25,8 +52,7 @@ $('#trial-form').submit(function(event) {
   $submitButton.prop('disabled', true);
   $('#error-message').addClass('d-none');
 
-  var url = $form.attr('action');
-  $.post(url, {
+  $.post('https://api.cryptomator.org/licenses/create.php', {
     installation_id: $('#installation-id').val(),
     subject_email: $('#email').val(),
     expiration_notification: $('#email-consent').is(':checked') ? 1 : 0
@@ -45,6 +71,39 @@ $('#trial-form').submit(function(event) {
   })
   .fail(function(jqXHR, textStatus, errorThrown) {
     console.error('Failed to request trial license.', textStatus, errorThrown);
+    $('#error-message').removeClass('d-none');
+    $submitButton.prop('disabled', false);
+  });
+});
+
+$('#upgrade-trial-form').submit(function(event) {
+  event.preventDefault();
+
+  var $form = $(this);
+  var $submitButton = $form.find('button[type=submit]');
+  $submitButton.prop('disabled', true);
+  $('#error-message').addClass('d-none');
+
+  $.post('https://api.cryptomator.org/licenses/upgrade.php', {
+    installation_id: $('#installation-id').val(),
+    license_id: $('#license-id').val(),
+    subject_email: $('#email').val(),
+    expiration_notification: $('#email-consent').is(':checked') ? 1 : 0
+  })
+  .done(function(data) {
+    $('#license-key').val(data.jwt);
+    $('#form-container').addClass('d-none');
+    $('#success-container').removeClass('d-none');
+
+    var successUrl = getUrlParameter('success_url');
+    if (successUrl) {
+      var $redirectForm = $('#success-redirect-form');
+      $redirectForm.prop('action', successUrl);
+      $redirectForm.submit();
+    }
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.error('Failed to upgrade trial license.', textStatus, errorThrown);
     $('#error-message').removeClass('d-none');
     $submitButton.prop('disabled', false);
   });
