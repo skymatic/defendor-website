9594bbf0037425593613c918e3830cc5e1f8c08a
Tobias Hagemann <tobias@hagemann.org>
2018-11-04 18:06:10 +0100

    added newsletter

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+$.ajaxSetup({
+  xhrFields: {
+    withCredentials: true
+  }
+});
+
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
@@ -44,6 +50,37 @@ $(document).ready(function () {
       $email.prop('readonly', false);
     });
   }
+});
+
+$('#newsletter-form').submit(function(event) {
+  event.preventDefault();
+
+  const $form = $(this);
+  const $submitButton = $form.find('button[type=submit]');
+  $submitButton.prop('disabled', true);
+  $('#error-message').addClass('d-none');
+
+  $.get('https://api.cryptomator.org/mailtrain/subscribe.php')
+  .done(function(data) {
+    $.post('https://api.cryptomator.org/mailtrain/subscribe.php', {
+      listid: 'Hkrk3qn37',
+      email: $('#email').val()
+    })
+    .done(function(data) {
+      $form.addClass('d-none');
+      $('#success-message').removeClass('d-none');
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      console.error('Failed to subscribe to newsletter.', textStatus, errorThrown);
+      $('#error-message').removeClass('d-none');
+      $submitButton.prop('disabled', false);
+    });
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.error('Failed to subscribe to newsletter.', textStatus, errorThrown);
+    $('#error-message').removeClass('d-none');
+    $submitButton.prop('disabled', false);
+  });
 });
 
 $('#trial-form').submit(function(event) {
